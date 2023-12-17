4157c0094ac56c6cc93ed70f42b6b87b9562a5b4
vignesh222002 <gtavignesh22042002@gmail.com>
2023-12-17 14:54:22 +0530

    Initial Setup

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { config } from "dotenv";
+import authRouter from "./src/routes/auth.js";
+import { db } from "./src/db/index.js";
 
 const app = express();
 app.use(express.json());
 config();
 
+app.use('/auth', authRouter);
 
-app.listen(process.env.PORT, () => console.log(`Server listening in the Port ${process.env.PORT}`))
+db.connect((err) => {
+    if (err) {
+        console.log("Mysql connection error", err);
+    }
+    else {
+        console.log("Mysql connected Sucessfully")
+    }
+});
+
+app.listen(process.env.PORT, () => console.log(`Server listening in the Port ${process.env.PORT}`));
---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mysql from "mysql";
+import { config } from "dotenv";
+
+config();
+
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306,
+})
