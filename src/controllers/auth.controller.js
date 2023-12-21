5ca1bb21cbbcf2c34abb6fe6761d3558b6b144f4
vignesh222002 <gtavignesh22042002@gmail.com>
2023-12-21 17:49:58 +0530

    Add Redis

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,22 @@
 import { db } from "../db/index.js";
 import jwt from "jsonwebtoken";
 
-export async function login(purpose, field) {
+export async function isUserExist(field, value) {
     try {
-        let [rows, columns] = await db.query(`SELECT phone_number, email FROM users where ${purpose} = '${field}';`);
+        let [rows, columns] = await db.query(`select count(*) as user_exists from users where ${field} = '${value}'`)
+        if (rows[0].user_exists > 0) {
+            return true;
+        }
+        else return false
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+export async function login(field, value) {
+    try {
+        let [rows, columns] = await db.query(`SELECT phone_number, email FROM users where ${field} = '${value}';`);
         if (rows.length === 0) {
             throw new Error("User doesn't Exist")
         }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,38 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
-import { db } from "../db/index.js";
 import { config } from "dotenv";
-import { login } from "../controllers/auth.controller.js";
+import { isUserExist, login } from "../controllers/auth.controller.js";
+import { createClient } from "redis";
 
 const router = Router();
 config();
+
+const redisClient = createClient();
+const defaultExpiration = 10;
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+
+await redisClient.connect();
+
+router.post('/auth', async (request, response) => {
+    // Query.by = 'email' | 'phone_number'
+    if (request.query.by === 'email' | request.query.by === 'phone_number') {
+        // Body.email contains a valid email address
+
+        try {
+            if (isUserExist(request.query.by, request.body[request.query.by])) {
+                const otp = Math.floor(Math.random() * 1000000)
+                response.status(200).send(`user exist, ${otp}`)
+            }
+            else response.status(500).send({ status: false, message: "User Doesn't exist - You are accessing Signup Process" })
+        }
+        catch (error) {
+            response.status(500).send({ status: false, error, message: error.message })
+        }
+    }
+    else {
+        response.status(500).send({ status: false, error: "Send the Query Correctly like, by = 'email' | 'phone_number'" })
+    }
+})
 
 router.post('/login', async (request, response) => {
     // Query.by = 'email' | 'phone_number'
@@ -26,7 +53,7 @@ router.post('/login', async (request, response) => {
 })
 
 router.post('/verifyOtp', async (request, response) => {
-    
+
 })
 
 // router.get('/getUser', (request, response) => {
